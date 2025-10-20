622781fe963ea364b93c7f98f73aac4b6db7dce0
egormir2188 <yaegormir2188@gmail.com>
2025-10-20 12:37:42 +0400

    Знакомство с API клиентами. Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,27 @@
+from httpx import Response
+
+from clients.api_client import APIClient
+from typing import TypedDict
+
+
+class UsersRequestDict(TypedDict):
+    """
+    Описание структуры запроса для создания пользователя
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с /api/v1/users
+    """
+    def create_users_api(self, request: UsersRequestDict) -> Response:
+        """
+        Метод выполняет регистрацию пользователя
+        :param request: Словарь с ключами email, password, lastName, firstName, middleName
+        :return:
+        """
+        return self.client.post('/api/v1/users', json=request)
