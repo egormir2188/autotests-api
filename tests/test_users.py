6d612189ca9e635a0b5af085508b9d080379fa4f
egormir2188 <yaegormir2188@gmail.com>
2025-10-29 14:37:52 +0400

    8.5 Фикстуры Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,7 @@
 import pytest
 from http import HTTPStatus
 
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.base import assert_status_code
@@ -9,9 +9,7 @@ from tools.assertions.users import assert_create_user_response
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user():
-    public_users_client =get_public_users_client()
-
+def test_create_user(public_users_client: PublicUsersClient):
     create_user_request = CreateUserRequestSchema()
     create_user_response = public_users_client.create_user_api(create_user_request)
     create_user_response_data = CreateUserResponseSchema.model_validate_json(create_user_response.text)
@@ -20,4 +18,3 @@ def test_create_user():
     assert_create_user_response(create_user_request, create_user_response_data)
 
     validate_json_schema(create_user_response.json(), create_user_response_data.model_json_schema())
-
