466aaf396289769cae11940eb3c808d8fff89345
egormir2188 <yaegormir2188@gmail.com>
2025-10-31 22:16:50 +0400

    9.4 Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Обновление задания

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,5 +1,5 @@
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    GetExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
 from tools.assertions.base import assert_equal
 from clients.exercises.exercises_schema import ExerciseSchema
 
@@ -51,3 +51,24 @@ def assert_get_exercise_response(
     :raises AssertionError: Если данные курсов не совпадают.
     """
     assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
+
+def assert_update_exercise_response(request: UpdateExerciseRequestSchema, response: UpdateExerciseResponseSchema):
+    """
+    Проверяет, что ответ на обновление задания соответствует данным из запроса.
+
+    :param request: Исходный запрос на обновление задания.
+    :param response: Ответ API с обновленными данными задания.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    if request.title is not None:
+        assert_equal(response.exercise.title, request.title, 'title')
+    if request.description is not None:
+        assert_equal(response.exercise.description, request.description, 'description')
+    if request.max_score is not None:
+        assert_equal(response.exercise.max_score, request.max_score, 'max_score')
+    if request.min_score is not None:
+        assert_equal(response.exercise.min_score, request.min_score, 'min_score')
+    if request.estimated_time is not None:
+        assert_equal(response.exercise.estimated_time, request.estimated_time, 'estimated_time')
+    if request.order_index is not None:
+        assert_equal(response.exercise.order_index, request.order_index, 'order_index')
