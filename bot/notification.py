6d7ef5ef614a3d7f4884e9c15dc4f61d3c16757d
egormir2188 <yaegormir2188@gmail.com>
2025-11-27 17:53:45 +0400

    Update notification.py

--- bot/notification.py
+++ bot/notification.py
@@ -1,6 +1,7 @@
-from telegram import Bot
-import asyncio
+import asyncio, shutil
 
+from datetime import datetime
+from telegram import Bot
 from tools.allure.formating_message import parse_allure_report, format_telegram_message
 from config import BotConfig
 
@@ -8,8 +9,18 @@ from config import BotConfig
 passed, failed, broken,failed_tests = parse_allure_report('allure-report')
 message = format_telegram_message(passed, failed, broken, failed_tests)
 
-async def send_telegram_message_async():
-    bot = Bot(token=BotConfig.bot_token)
-    await bot.send_message(chat_id=BotConfig.chat_id, text=message, parse_mode='Markdown')
+current_time = datetime.now()
+formatted_time = current_time.strftime('%Y-%m-%d_%H-%M-%S')
+file_name = f'allure-report_{formatted_time}'
 
-asyncio.run(send_telegram_message_async())
+shutil.make_archive(file_name, 'zip', 'allure-report')
+
+async def send_telegram_test_artifacts_async(file_path: str, caption: str):
+    bot = Bot(token=BotConfig.bot_token)
+    with open(file_path, 'rb') as file:
+        await bot.send_document(chat_id=BotConfig.chat_id, document=file, caption=caption, parse_mode='Markdown')
+
+def send_telegram_artifacts(file_path: str, caption: str):
+    asyncio.run(send_telegram_test_artifacts_async(file_path, caption))
+
+send_telegram_artifacts(f'{file_name}.zip', message)
