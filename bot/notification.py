60e3aa6e72e7e05d561bd0c305be45c5d5a6965d
egormir2188 <yaegormir2188@gmail.com>
2025-11-27 14:40:28 +0400

    Update workflow for tg notifications

--- bot/notification.py
+++ bot/notification.py
@@ -7,5 +7,4 @@ def send_telegram_message(message: str):
     url = f'{BotConfig.telegram_host}/{BotConfig.send_message_uri}'
     payload = {"chat_id": BotConfig.chat_id, "text": message}
 
-    response = httpx.post(url, data=payload)
-    print(response.status_code)
+    httpx.post(url, data=payload)
